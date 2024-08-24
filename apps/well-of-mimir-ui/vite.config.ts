cecb3b4ca1ac7d00dc6bfedda36c9efffa0fcad2
Balint Orosz <orosz.balint.97@gmail.com>
2024-08-24 10:43:04 +0200

    initial setup

--- apps/well-of-mimir-ui/vite.config.ts
+++ apps/well-of-mimir-ui/vite.config.ts
@@ -1,9 +1,20 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/well-of-mimir-ui',
+      provider: 'v8',
+    },
+  },
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/well-of-mimir-ui',
 
---
+++ libs/task-management/README.md
@@ -0,0 +1,11 @@
+# task-management
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build task-management` to build the library.
+
+## Running unit tests
+
+Run `nx test task-management` to execute the unit tests via [Vitest](https://vitest.dev/).
